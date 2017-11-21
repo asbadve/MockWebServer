e8abee0f89f08e8c3d8790c0239567c9e9463242
ajinkya <ajinkya.badve@xpressbees.com>
2017-11-21 18:45:54 +0530

    Custom Test Runner added

--- MockWebServer/app/src/androidTest/java/com/example/ajinkyabadve/mockwebserver/MainActivityTest.java
+++ MockWebServer/app/src/androidTest/java/com/example/ajinkyabadve/mockwebserver/MainActivityTest.java
@@ -1,11 +1,15 @@
 package com.example.ajinkyabadve.mockwebserver;
 
+import android.support.test.InstrumentationRegistry;
 import android.support.test.rule.ActivityTestRule;
 
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
+
+import okhttp3.mockwebserver.MockResponse;
+import okhttp3.mockwebserver.MockWebServer;
 
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
@@ -20,15 +24,27 @@ public class MainActivityTest {
     public ActivityTestRule<MainActivity> rule = new ActivityTestRule<>(
             MainActivity.class,
             true,
-            true);
+            false);
     @Rule
     public OkHttpIdlingResourceRule okHttpIdlingResourceRule = new OkHttpIdlingResourceRule();
 
     @Test
     public void followers() throws Exception {
+        MockWebServer mockWebServer = new MockWebServer();
+        mockWebServer.start();
+
+        TestDemoAppClass app = (TestDemoAppClass) InstrumentationRegistry.getTargetContext().getApplicationContext();
+        app.setBaseUrl(mockWebServer.url("/").toString());
+
+        mockWebServer.enqueue(new MockResponse().setBody(
+                "{\"login\" :\"octocat\",\"followers\" :\"2011\" "
+        ));
+        rule.launchActivity(null);
+
         onView(withId(R.id.followers))
                 .check(matches(withText("octocat: 2011")));
 
+        mockWebServer.shutdown();
     }
 
     @Before
