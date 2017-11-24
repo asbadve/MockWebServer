6987fb181cebf78f12de995666b1c188b5dbd18c
ajinkya <ajinkya.badve@xpressbees.com>
2017-11-24 11:05:14 +0530

    error conditions tested

--- MockWebServer/app/src/androidTest/java/com/example/ajinkyabadve/mockwebserver/MainActivityTest.java
+++ MockWebServer/app/src/androidTest/java/com/example/ajinkyabadve/mockwebserver/MainActivityTest.java
@@ -7,6 +7,8 @@ import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
 
+import java.util.concurrent.TimeUnit;
+
 import okhttp3.mockwebserver.MockResponse;
 
 import static android.support.test.espresso.Espresso.onView;
@@ -19,6 +21,8 @@ import static android.support.test.espresso.matcher.ViewMatchers.withText;
  * Created by ajinkyabadve on 21/11/17.
  */
 public class MainActivityTest {
+    private static final String OCTOCAT_BODY = "{ \"login\" : \"octocat\", \"followers\" : 1500 }";
+
     @Rule
     public ActivityTestRule<MainActivity> activityRule
             = new ActivityTestRule<>(MainActivity.class, true, false);
@@ -38,13 +42,43 @@ public class MainActivityTest {
 
     @Test
     public void followers() {
-        mockWebServerRule.server.enqueue(new MockResponse().setBody(
-                "{ \"login\" : \"octocat\", \"followers\" : 1500 }"));
+        mockWebServerRule.server.enqueue(new MockResponse().setBody(OCTOCAT_BODY));
 
         activityRule.launchActivity(null);
 
         onView(withId(R.id.followers))
                 .check(matches(withText("octocat: 1500")));
+    }
+
+    @Test
+    public void status404() {
+        mockWebServerRule.server.enqueue(new MockResponse().setResponseCode(404));
+
+        activityRule.launchActivity(null);
+
+        onView(withId(R.id.followers))
+                .check(matches(withText("404")));
+    }
+
+    @Test
+    public void malformedJson() {
+        mockWebServerRule.server.enqueue(new MockResponse().setBody("Jason"));
+
+        activityRule.launchActivity(null);
+
+        onView(withId(R.id.followers))
+                .check(matches(withText("JsonEncodingException")));
+    }
+
+    @Test
+    public void timeout() {
+        mockWebServerRule.server.enqueue(
+                new MockResponse().setBody(OCTOCAT_BODY).throttleBody(1, 1, TimeUnit.SECONDS));
+
+        activityRule.launchActivity(null);
+
+        onView(withId(R.id.followers))
+                .check(matches(withText("SocketTimeoutException")));
     }
 
 
